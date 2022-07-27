4e891a16ebc2ab5c2f2e5a778ecda117f6bd47e3
Shakeel Majeed <shakeelsmajeed@outlook.com>
2022-07-27 19:45:00 +0100

    Revamping

--- src/index.js
+++ src/index.js
@@ -2,58 +2,100 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import me from './images/Profile Pic2.png';
-//import {borders} from '@material-ui/core';
-import Box from '@material-ui/core/Box';
+
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import MailIcon from '@material-ui/icons/Mail';
-import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/styles';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 
+import { Accordion } from '@mui/material';
+import { AccordionDetails, AccordionSummary } from '@mui/material';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {Typography} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  bottom: { //for the 'invisible' footer with mail and LinkedIn icons 
+    backgroundColor: 'rgba(202, 213, 226, 1)'
+  },
+  info: {
+    width: '100%',
+  }
+
+})
 
 function Image(props){
   return (
   <div style={{ textAlign: 'center'}}>
-    <img src={me} alt="A picture of Shakeel Majeed"/>
+    <img src={me} alt="A photo of Shakeel Majeed"/>
   </div>
   );
 }
-//const image = <Image/>;
+
 function Text(props){
-  return (
-  <div style={{ textAlign: 'center'}}>
-    <h1>Shakeel Majeed</h1>
-    <br></br>
-    <hr></hr>
-      <div>
-          <strong>About me: </strong>Since a young age, I have been prudent in absorbing information on Data Structures and Algorithms and Economics. Having built a Full Stack Web App and written an essay on how Oligopolies and Monopolies operate, I know putting my faith and effort into my interests will lay the foundations for my future. Furthermore, I aspire to be a SWE.
+  const classes = useStyles()
+  return (  
+    <div style={{fontSize:'1vw'}}>
+     <div style={{textAlign: 'center'}}>
+       <h1>Shakeel Majeed</h1>
+       <hr></hr>
+       <div style={{textAlign:'left'}}>
+        <Accordion className={classes.info}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header">
+              <h3>About Me:</h3>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>Since a young age, I have been prudent in absorbing information on Data Structures and Algorithms and Economics. Having built a full stack blogging Web App, written an essay on how Oligopolies and Monopolies operate (prior to taking GCSE Economics) and being a frequent user of sites like USACO and Leetcode, I know putting my faith and effort into my interests will lay the foundations for my future. Furthermore, I aspire to be a SWE or a Quant.</Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion className={classes.info}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header">
+              <h3>Technologies:</h3>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>Python (including Django), JavaScript</Typography>
+              <a href="https://github.com/shakeelmajeed-work">GitHub</a>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion className={classes.info}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header">
+              <h3>CV:</h3>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>This CV is more of a document currently for me (and you) to see what I've done so far hence it being 2 pages and not summarised as much as it could be. <a href="https://1drv.ms/b/s!ApuSg4CyvQqDzmY8WNHv6N6n3Gn0">Click here for a PDF version of my CV</a></Typography>
+            </AccordionDetails>
+          </Accordion>
+          </div>
+        </div>
       </div>
-      <hr></hr>
-      <div style={{textAlign:'left'}}>
-        <Box fontWeight="fontWeightRegular" component="span" display="inline-block">
-            <strong>Technologies:</strong> Python (including Django), JavaScript (including React but ongoing), <a href="https://github.com/shakeelmajeed-work">Github</a>
-        </Box>  
-      </div>
-      <hr></hr>
-      <div style={{textAlign:'left'}}>
-        <Box fontWeight="fontWeightRegular" component="span" display="inline-block">
-            <strong>Resume: </strong><a href="https://drive.google.com/file/d/1GfgectcYlR6zP-JY-vehviky947t3Vo1/view?usp=sharing">Click here</a>
-        </Box>
-      </div>
-      <hr></hr>
-  </div>
-  
   );
 }
 
 function Links(props){
+  const classes = useStyles()
   return(
-    <div id="footer" style={{textAlign:'center'}}>
-      <a href="https://www.linkedin.com/in/shakeel-majeed/" class="fa fa-linkedin"></a>
-      <a href="mailto:17majeed_s@bexleygs.co.uk" class="fa fa-google"></a>
-      </div>
+    <BottomNavigation showLabels className={classes.bottom} sx={{ position: "fixed", bottom: 0 }}>
+      <a href="https://www.linkedin.com/in/shakeel-majeed/">
+        <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon/>}/>
+      </a>
+      <a href="mailto:shakeelsmajeed@outlook.com">
+        <BottomNavigationAction label="Email" icon={<MailIcon/>} />
+      </a>
+    </BottomNavigation>
   );
 }
 
-const text = <Text />;
+
 ReactDOM.render(
   <React.Fragment>
     <Image />
